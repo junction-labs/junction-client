265a2d18d98e4b3c239cde70ee8de5cf207c8c06
Ben Linsay <blinsay@gmail.com>
2024-09-27 18:43:49 -0400

    add data to errors
    start writing better routing error messages by including details on
    which routes and backends a request mapped to. also includes info on
    which routing rule matched, but we're not doing anything with that yet.

--- crates/junction-core/src/error.rs
+++ crates/junction-core/src/error.rs
@@ -1,3 +1,5 @@
+use junction_api_types::shared::Attachment;
+
 /// A `Result` alias where the `Err` case is `junction_core::Error`.
 pub type Result<T> = std::result::Result<T, Error>;
 
@@ -6,30 +8,58 @@ pub enum Error {
     #[error("invalid url: {0}")]
     InvalidUrl(&'static str),
 
-    #[error("ADS connection lost, client has crashed")]
-    AdsConnectionLost,
+    #[error("invalid route configuration")]
+    InvalidRoutes {
+        message: &'static str,
+        attachment: Attachment,
+        rule: usize,
+    },
 
-    #[error("no route matched the hostname and port")]
-    NoRouteMatched,
+    #[error("invalid backend configuration")]
+    InvalidBackends {
+        message: &'static str,
+        attachment: Attachment,
+    },
 
-    #[error("no rule within the matching route matched the request parameters")]
-    NoRuleMatched,
+    #[error(
+        "no routing info is available for any of the following targets: [{}]",
+        format_attachments(.routes)
+    )]
+    NoRouteMatched { routes: Vec<Attachment> },
 
-    #[error("the matched backend does not have any endpoints")]
-    NoEndpoints,
+    #[error("using route '{route}': no routing rules matched the request")]
+    NoRuleMatched { route: Attachment },
 
-    #[error("the matched backend does not have any reachable endpoints")]
-    NoReachableEndpoints,
+    #[error("{route}: backend not found: {backend}")]
+    NoBackend {
+        route: Attachment,
+        rule: usize,
+        backend: Attachment,
+    },
 
-    #[error("the passed configuration has routes with no backends defined")]
-    BackendListEmpty,
+    #[error("{backend}: no endpoints are healthy")]
+    NoReachableEndpoints {
+        route: Attachment,
+        backend: Attachment,
+    },
+}
+
+fn format_attachments(attachments: &[Attachment]) -> String {
+    attachments
+        .iter()
+        .map(|a| a.to_string())
+        .collect::<Vec<_>>()
+        .join(", ")
 }
 
 impl Error {
     pub(crate) fn is_temporary(&self) -> bool {
         matches!(
             self,
-            Error::NoRouteMatched | Error::NoRuleMatched | Error::NoEndpoints
+            Error::NoRouteMatched { .. }
+                | Error::NoBackend { .. }
+                | Error::NoRuleMatched { .. }
+                | Error::NoReachableEndpoints { .. }
         )
     }
 }
