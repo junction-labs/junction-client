894014dff170a6b50aee29eb2b5c769661266c61
Ben Linsay <blinsay@gmail.com>
2024-12-27 15:30:36 -0500

    xtask: node tasks and some README updates

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -9,28 +9,36 @@ fn main() -> anyhow::Result<()> {
 
     let venv = env::var("VIRTUAL_ENV").unwrap_or_else(|_| ".venv".to_string());
 
+    use Commands::*;
     match &args.command {
         // rust
-        Commands::CITest => rust::ci_test(&sh),
-        Commands::CIDoc => rust::ci_doc(&sh),
-        Commands::CIClippy {
+        CITest => rust::ci_test(&sh),
+        CIDoc => rust::ci_doc(&sh),
+        CIClippy {
             crates,
             fix,
             allow_staged,
         } => rust::ci_clippy(&sh, crates, *fix, *allow_staged),
+        // node
+        NodeBuild => node::build(&sh),
+        NodeClean => node::clean(&sh),
+        NodeLint { fix } => node::lint(&sh, *fix),
+        NodeShell => node::shell(&sh),
         // python
-        Commands::PythonClean => python::clean(&sh, &venv),
-        Commands::PythonBuild {
+        PythonBuild {
             maturin,
             skip_stubs,
         } => python::build(&sh, &venv, maturin, !*skip_stubs),
-        Commands::PythonLint { fix } => python::lint(&sh, &venv, *fix),
-        Commands::PythonTest => python::test(&sh, &venv),
-        Commands::PythonDocs => python::docs(&sh, &venv),
+        PythonClean => python::clean(&sh, &venv),
+        PythonDocs => python::docs(&sh, &venv),
+        PythonLint { fix } => python::lint(&sh, &venv, *fix),
+        PythonTest => python::test(&sh, &venv),
+        PythonShell => python::shell(&sh, &venv),
     }
 }
 
 /// Cargo xtasks for development.
+///
 ///
 /// xtasks are here anything common that takes more than a single, standard
 /// cargo command. If a cargo default isn't working, try running `cargo xtask
@@ -76,7 +84,30 @@ enum Commands {
     /// features set for public docs.
     CIDoc,
 
+    /// Build the Node native extension and compile typescript.
+    ///
+    /// Does not build a release build. To build a release build in CI, use
+    /// Cargo and npm directly.
+    NodeBuild,
+
+    /// Clean up the current node_modules and remove any built native
+    /// extensions.
+    NodeClean,
+
+    /// Lint and format Typescript and Javascript.
+    NodeLint {
+        /// Try to automatically fix any linter/formatter errors.
+        #[clap(long)]
+        fix: bool,
+    },
+
+    /// Run a `node` repl. Builds a fresh debug version of Junction Node before
+    /// starting the shell.
+    NodeShell,
+
     /// Build and install junction-python in a .venv.
+    ///
+    /// Does not build a release build. In CI, use maturin directly.
     PythonBuild {
         /// Skip rebuilding the junction-python wheel. Useful for working on
         /// generating config type information.
@@ -89,8 +120,11 @@ enum Commands {
         skip_stubs: bool,
     },
 
-    /// Run junction-python's Python tests.
-    PythonTest,
+    /// Clean the current virtualenv and any Python caches.
+    PythonClean,
+
+    /// Build Python docs with sphinx.
+    PythonDocs,
 
     /// Lint and format Python code.
     PythonLint {
@@ -99,11 +133,12 @@ enum Commands {
         fix: bool,
     },
 
-    /// Clean the current virtualenv and any Python caches.
-    PythonClean,
+    /// Run a `python` repl in the current virtual environment. Builds a fresh
+    /// version of Junction Python and installs it before starting.
+    PythonShell,
 
-    /// Build Python docs with sphinx.
-    PythonDocs,
+    /// Run junction-python's Python tests.
+    PythonTest,
 }
 
 mod rust {
@@ -189,6 +224,8 @@ fn loud_env<K: AsRef<OsStr>, V: AsRef<OsStr>>(sh: &Shell, key: K, value: V) -> x
 }
 
 mod python {
+    use std::os::unix::process::CommandExt;
+
     use super::*;
 
     pub(super) fn clean(sh: &Shell, venv: &str) -> anyhow::Result<()> {
@@ -218,6 +255,14 @@ mod python {
         }
 
         Ok(())
+    }
+
+    pub(super) fn shell(sh: &Shell, venv: &str) -> anyhow::Result<()> {
+        ensure_venv(sh, venv)?;
+        build(sh, venv, &MaturinOption::Develop, true)?;
+
+        let mut cmd: std::process::Command = cmd!(sh, "{venv}/bin/python").into();
+        Err(cmd.exec().into())
     }
 
     fn maturin_develop(sh: &Shell, venv: &str) -> anyhow::Result<()> {
@@ -347,5 +392,49 @@ mod python {
         .run()?;
 
         Ok(())
+    }
+}
+
+mod node {
+    use std::os::unix::process::CommandExt;
+
+    use super::*;
+
+    pub(super) fn clean(sh: &Shell) -> anyhow::Result<()> {
+        cmd!(sh, "rm -rf junction-node/node_modules/").run()?;
+        cmd!(sh, "rm -rf junction-node/index.node").run()?;
+        cmd!(sh, "rm -rf junction-node/platforms/**/index.node").run()?;
+
+        Ok(())
+    }
+
+    pub(super) fn build(sh: &Shell) -> anyhow::Result<()> {
+        let _dir = sh.push_dir("junction-node");
+
+        cmd!(sh, "npm install --fund=false").run()?;
+        cmd!(sh, "npm run build").run()?;
+
+        Ok(())
+    }
+
+    pub(super) fn lint(sh: &Shell, fix: bool) -> anyhow::Result<()> {
+        let _dir = sh.push_dir("junction-node");
+
+        cmd!(sh, "npm install --fund=false").run()?;
+
+        let lint_cmd = if fix { "fix" } else { "lint" };
+        cmd!(sh, "npm run {lint_cmd}").run()?;
+
+        Ok(())
+    }
+
+    pub(super) fn shell(sh: &Shell) -> anyhow::Result<()> {
+        let _dir = sh.push_dir("junction-node");
+
+        cmd!(sh, "npm install --fund=false").run()?;
+        cmd!(sh, "npm run build").run()?;
+
+        let mut cmd: std::process::Command = cmd!(sh, "node").into();
+        Err(cmd.exec().into())
     }
 }
